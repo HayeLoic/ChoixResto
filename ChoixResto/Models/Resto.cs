227932064ca696a31b95a31e1f0bccf57896c724
LHAYE <lhaye@avocatparis.org>
2016-10-07 12:20:03 +0200

    Nettoyage du code avant le TP

--- ChoixResto/Models/Resto.cs
+++ ChoixResto/Models/Resto.cs
@@ -12,8 +12,8 @@ namespace ChoixResto.Models
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "Le nom du restaurant doit être saisi")]
-        [StringLength(80)]
         public string Nom { get; set; }
+        [Display(Name = "Téléphone")]
         [RegularExpression(@"^0[0-9]{9}$", ErrorMessage = "Le numéro de téléphone est incorrect")]
         public string Telephone { get; set; }
     }
