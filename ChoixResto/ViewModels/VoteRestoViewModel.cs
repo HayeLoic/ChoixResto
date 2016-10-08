ae465f46071c9510595a6eab00998c4e23a0a806
HayeLoic <haye.loic@gmail.com>
2016-10-08 14:54:56 +0200

    Page de vote la création de sondage pointe vers l'index de vote pour un resto

---
+++ ChoixResto/ViewModels/VoteRestoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChoixResto.ViewModels
+{
+    public class VoteRestoViewModel
+    {
+        public Models.Resto Resto { get; set; }
+        public bool RestoChecked { get; set; }
+    }
+}
